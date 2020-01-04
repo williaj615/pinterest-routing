2e1e6a16b083f22c0090119d4896593d58ae868b
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-04 10:12:22 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Bootstrap Button</button>
+    </div>
+  );
+}
+
+export default App;
