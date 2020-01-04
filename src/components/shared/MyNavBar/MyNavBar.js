b33ffe7f9346646e6d4ee17a79bc78eec12a2bdc
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-04 13:24:10 -0600

    logout button works

--- src/components/shared/MyNavBar/MyNavBar.js
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 class MyNavBar extends React.Component {
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
+    const { authed } = this.props;
     return (
       <div className="MyNavBar">
-        <h1>IMA NAVBAR</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">Pinterest</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+            <div class="my-2 mt-lg-0">
+              {authed && (<button className="nav-linsk btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
