2286b842d453b4b316a82fb612d7c7f7f72e5d26
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-07 19:18:39 -0600

    navbar looks killer

--- src/components/shared/MyNavBar/MyNavBar.js
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MyNavBar extends React.Component {
   static propTypes = {
@@ -15,18 +16,35 @@ class MyNavBar extends React.Component {
 
   render() {
     const { authed } = this.props;
+
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Boards</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="board/new">New Board</Link>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-danger onClick={this.logMeOut}">Logout</button>
+            </li>
+          </ul>
+        );
+      }
+
+      return (<ul className="navbar-nav ml-auto"></ul>);
+    };
     return (
       <div className="MyNavBar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">Pinterest</a>
+          <Link className="navbar-brand" to="/">Pinterest</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-            <div className="my-2 mt-lg-0">
-              {authed && (<button className="nav-linsk btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
-            </div>
+              { buildNavbar() }
           </div>
         </nav>
       </div>
